8df00d3bb42e3fa63c9ad434af1bd83a5ef1920d
Gustavo-program <gustavo.roman2@outlook.es>
2021-11-09 11:44:11 -0500

    empezando

---
+++ src/main/java/com/modelo/peruapps/TraineePeruAppsApplication.java
@@ -0,0 +1,13 @@
+package com.modelo.peruapps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraineePeruAppsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TraineePeruAppsApplication.class, args);
+	}
+
+}
